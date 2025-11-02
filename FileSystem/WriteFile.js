67499cd5c9e0032331e0e339f9b1db1b190485e7
Hariprakash3131 <hariprakash3131@gmail.com>
2025-11-03 00:05:03 +0530

    file6

--- FileSystem/WriteFile.js
+++ FileSystem/WriteFile.js
@@ -9,3 +9,13 @@ wf.writeFile('./FolderCrete/file1.txt','Create The File',(err)=>{
         console.log('File created')
     }
 })
+
+
+wf.readFile('./FolderCrete/file1.txt',(err,data)=>{
+    if(err){
+        console.log(err.message)
+    }
+    else{
+        console.log(data.toString())
+    }
+})
