11207da42ccd90343939e9247e85fc42ca5c0717
Hariprakash3131 <168099983+Hariprakash3131@users.noreply.github.com>
2025-11-09 23:01:45 +0530

    Implement folder and file deletion in FileSystem.js

--- FileSystem.js
+++ FileSystem.js
@@ -1,1 +1,61 @@
+//File System
 
+const fs=require('fs')
+
+//Create The Folder
+if(! fs.existsSync('./FileSystemFolder')){
+    fs.mkdir('./FileSystemFolder',(err)=>{
+    if(err){
+        console.log(err.message)
+    }
+    else{
+      console.log('Folder Created')
+    }
+    
+   
+})
+   console.log('mkdir is async function')
+}
+
+
+//Create the File
+
+// fs.writeFile('./FileSystemFolder/file1.txt','Try to create the file',(err)=>{
+//     if(err){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log('File Created')
+//     }
+// })
+
+//Read the File
+// if (fs.existsSync('./FileSystemFolder/file1.txt')){
+//     fs.readFile('./FileSystemFolder/file1.txt',(err,data)=>{
+//     if(err){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log(data.toString())
+//     }
+// })
+// }
+
+//Delete The File
+if(fs.existsSync('./FileSystemFolder/file1.txt')){
+    fs.unlink('./FileSystemFolder/file1.txt',(err)=>{
+        if(err){
+            console.log(err.message)
+        }
+        else{
+            console.log('File Deleted')
+        }
+    })
+}
+
+fs.rmdir('./FileSystemFolder',(err)=>{
+    if(err){
+        console.log(err.message)
+    }
+    console.log('Folder Deleted')
+})
