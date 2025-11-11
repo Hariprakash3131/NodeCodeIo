add1b87375ee74547aaded6d2bc63a42a5bbf3b1
Hariprakash3131 <hariprakash3131@gmail.com>
2025-11-11 13:08:57 +0530

    server

---
+++ server.js
@@ -0,0 +1,8 @@
+const http=require('http')
+const server=http.createServer((req,res)=>{
+    console.log('Request Mode')
+})
+
+server.listen(3000,'localhost',()=>{
+    console.log("Server Listening")
+})
