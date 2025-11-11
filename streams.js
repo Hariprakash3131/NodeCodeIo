acdec9c3c1981d1423e227dfb4228effc1dc7e53
Hariprakash3131 <hariprakash3131@gmail.com>
2025-11-11 11:44:41 +0530

    streams

---
+++ streams.js
@@ -0,0 +1,16 @@
+const stream=require('fs')
+const readStream=stream.createReadStream('./Dummy/streams.txt',{encoding:'utf-8'})
+const writeStream=stream.createWriteStream('./Dummy/copyStreams.txt')
+
+readStream.on('data',(buffer)=>{
+       writeStream.write(buffer)
+})
+
+
+
+// readStream.on('data',(buffer)=>{    // on is event
+//     console.log('\n New Line \n')
+//       console.log('\n New Line \n')
+//         console.log('\n New Line \n')
+//     console.log(buffer)  //  console.log(buffer.toString())
+// })
