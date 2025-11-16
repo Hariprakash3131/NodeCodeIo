8903fa994615c38b8ce32ddbfe4c88932bc2d36b
Hariprakash3131 <hariprakash3131@gmail.com>
2025-11-16 12:35:00 +0530

    exp

--- Express/app1.js
+++ Express/app1.js
@@ -1,8 +1,17 @@
+// const exp=require('express')
+// const app=exp()
+
+// app.listen(5000)
+
+// app.get('/',(req,res)=>{
+//     res.send("James")
+// })
+
+
 const exp=require('express')
 const app=exp()
-
 app.listen(5000)
 
 app.get('/',(req,res)=>{
-    res.send("James")
+    res.sendFile('/Dummy/index.html')
 })
