8d1c4300a363af31150cdbc033b876afad405e7e
Hariprakash3131 <hariprakash3131@gmail.com>
2025-11-18 21:22:27 +0530

    local1

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
-names=['James','Virat','Watson']
-ages=[21,36,44]
-// console.log(names)
+// names=['James','Virat','Watson']
+// ages=[21,36,44]
+// // console.log(names)
 
-module.exports={
-    names,
-    ages
-}    //Multiple module exports
+// module.exports={
+//     names,
+//     ages
+// // }    //Multiple module exports
