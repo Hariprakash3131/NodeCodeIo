93f7a16ffbe067651c4ae720a7d42824d5d45b6c
Hariprakash3131 <hariprakash3131@gmail.com>
2025-11-19 13:43:16 +0530

    app response

--- Express/app1.js
+++ Express/app1.js
@@ -13,5 +13,22 @@ const app=exp()
 app.listen(5000)
 
 app.get('/',(req,res)=>{
-    res.sendFile('/Dummy/index.html')
+    res.sendFile('./index.html',{root:__dirname})
+})
+
+app.get('/soker',(req,res)=>{
+    res.sendFile('./home.html',{root:__dirname})
+})
+// app.get('/home',(req,res)=>{
+//     res.sendFile('./home.html',{root:__dirname})
+// })   This code not run redirect to the index.html the reason is : "/home" url 
+
+
+app.get('/about',(req,res)=>{
+    res.sendFile('./about.html',{root:__dirname})
+
+})
+
+app.get('/contact',(req,res)=>{
+    res.sendFile('./contact.html',{root:__dirname})
 })
